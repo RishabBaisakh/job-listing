f87ec04e182302a9d3882c9d75f95a1f56b3de00
RishabBaisakh <rishab.baisakh@gmail.com>
2025-08-04 14:26:13 -0400

    added vuetify + icons

--- src/main.js
+++ src/main.js
@@ -1,15 +1,30 @@
-import "./assets/main.css";
+import { createApp } from "vue";
+import App from "./App.vue";
 import "primeicons/primeicons.css";
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
-import router from "./router";
-import { createApp } from "vue";
-import App from "./App.vue";
 import store from "./store";
+import router from "./router";
+// styles
+import "./assets/main.css";
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 const app = createApp(App);
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+  },
+});
 
 app.use(store);
 app.use(router);
 app.use(Toast);
+app.use(vuetify);
 app.mount("#app");
